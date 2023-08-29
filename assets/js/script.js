dba956a54fd1a8f812090a09f9bed5c5dc21faae
Chandan Kumar Panigrahi <91644974+chandankumarpanigrahi@users.noreply.github.com>
2023-08-29 21:26:05 +0530

    Dark Mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
 // Dark Mode Toggle
 const toggleButton = document.getElementById("toggleButton");
 const body = document.body;
+const logoImage = document.getElementById('logoImage');
 
 toggleButton.addEventListener("click", () => {
     body.classList.toggle("dark");
     body.classList.toggle("light");
+    const currentSrc = logoImage.getAttribute('src');
+    const darkLogoSrc = './assets/images/logo_dark.png';
+    const lightLogoSrc = './assets/images/logo_light.png';
+    
+    if (currentSrc === darkLogoSrc) {
+        logoImage.setAttribute('src', lightLogoSrc);
+    } else {
+        logoImage.setAttribute('src', darkLogoSrc);
+    }
 });
